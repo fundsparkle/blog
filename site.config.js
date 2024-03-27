440d80766fb0cfa904fd875e3ef6f1c1457712ad
Przybysz <przybysz231@gmail.com>
2024-03-27 12:17:26 +0100

    change avatar.svg

--- site.config.js
+++ site.config.js
@@ -2,8 +2,8 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "dywidendowo.pl",
-    image: "notion-avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "inwestor",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Inwestor",
     bio: "Pokazuję swoją drogę do pasywnego dochodu z dywidend.",
     email: "fundsparkle-tech@gmail.com",
     github: "fundsparkle",
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://dywidendowo-blog.vercel.app/",
   since: 2024, // If leave this empty, current year will be used.
   lang: "pl-PL", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
