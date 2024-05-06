f6e799d6323dd22672a1813fe15583704212694a
Przybysz <przybysz231@gmail.com>
2024-05-06 17:17:20 +0200

    fixes

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,11 +1,14 @@
 export default function NewsletterSignUp() {
     return (
-        <div className='sign-up-container'>
-            <iframe width="100%" height="100%"
-                    src="https://6c25405f.sibforms.com/serve/MUIFAGh30jswQiwQXE3x0FqC2eIdS1d0Pfxu-7b0XcrMvJL9coqhxUkbyOD1w4uW-MrLMtvjGb7fpdGUYWPEvEjpWjcdVuG4FkBdKsQeytBQ6wJJGuj-jlweOQ5SA_kVyTE6896Da-LF7FNfEnOrlyTXUH0M766yKctLbro7ASgmXBy_pjgdcFdm5QE9Agk7Vy7hsjTMZYELXaxk"
-                    allowFullScreen
-                    style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%', maxHeight: '100%' }}>
-            </iframe>
-        </div>
+        <iframe width="100%" height="100%"
+                src="https://6c25405f.sibforms.com/serve/MUIFAGh30jswQiwQXE3x0FqC2eIdS1d0Pfxu-7b0XcrMvJL9coqhxUkbyOD1w4uW-MrLMtvjGb7fpdGUYWPEvEjpWjcdVuG4FkBdKsQeytBQ6wJJGuj-jlweOQ5SA_kVyTE6896Da-LF7FNfEnOrlyTXUH0M766yKctLbro7ASgmXBy_pjgdcFdm5QE9Agk7Vy7hsjTMZYELXaxk"
+                allowFullScreen
+                style={{
+                    display: 'block',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    maxWidth: '100%'
+                }}>
+        </iframe>
     )
 }
