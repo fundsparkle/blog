b58c4136fbddb532cce92def7f8f3d5bdfe708e1
Przybysz <przybysz231@gmail.com>
2024-05-06 17:53:42 +0200

    fixes

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -3,13 +3,14 @@ export default function NewsletterSignUp() {
         <div className="container" style={{position: 'relative'}}>
         <iframe width="100%"
                 src="https://6c25405f.sibforms.com/serve/MUIFAGh30jswQiwQXE3x0FqC2eIdS1d0Pfxu-7b0XcrMvJL9coqhxUkbyOD1w4uW-MrLMtvjGb7fpdGUYWPEvEjpWjcdVuG4FkBdKsQeytBQ6wJJGuj-jlweOQ5SA_kVyTE6896Da-LF7FNfEnOrlyTXUH0M766yKctLbro7ASgmXBy_pjgdcFdm5QE9Agk7Vy7hsjTMZYELXaxk"
-                allowFullScreen allowTransparency={true} frameBorder="0"
+                frameBorder="0"
+                scrolling="auto"
                 style={{
                     display: 'block',
                     marginLeft: 'auto',
                     marginRight: 'auto',
-                    maxWidth: '100%',
-                    height: "80vh"
+                    maxWidth: '80%',
+                    height: "100vh"
                 }}>
         </iframe>
         </div>
