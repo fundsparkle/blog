2c92f57f0952a54a63f2a4e13000f1ab06668340
Przybysz <przybysz231@gmail.com>
2024-05-06 17:40:01 +0200

    fixes

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,5 +1,6 @@
 export default function NewsletterSignUp() {
     return (
+        <div className="container" style={{position: 'relative'}}>
         <iframe width="100%" height="100%"
                 src="https://6c25405f.sibforms.com/serve/MUIFAGh30jswQiwQXE3x0FqC2eIdS1d0Pfxu-7b0XcrMvJL9coqhxUkbyOD1w4uW-MrLMtvjGb7fpdGUYWPEvEjpWjcdVuG4FkBdKsQeytBQ6wJJGuj-jlweOQ5SA_kVyTE6896Da-LF7FNfEnOrlyTXUH0M766yKctLbro7ASgmXBy_pjgdcFdm5QE9Agk7Vy7hsjTMZYELXaxk"
                 allowFullScreen
@@ -7,8 +8,10 @@ export default function NewsletterSignUp() {
                     display: 'block',
                     marginLeft: 'auto',
                     marginRight: 'auto',
-                    maxWidth: '100%'
+                    maxWidth: '100%',
+                    height: "100vh"
                 }}>
         </iframe>
+        </div>
     )
 }
