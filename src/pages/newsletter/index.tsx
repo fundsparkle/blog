4790901d5f3dd8a8a5831cc6228e53b0c95bdae2
Przybysz <przybysz231@gmail.com>
2024-05-06 16:59:41 +0200

    fixes

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -8,7 +8,7 @@ export default function NewsletterSignUp() {
             <iframe width="540" height="305"
                     src="https://6c25405f.sibforms.com/serve/MUIFAGh30jswQiwQXE3x0FqC2eIdS1d0Pfxu-7b0XcrMvJL9coqhxUkbyOD1w4uW-MrLMtvjGb7fpdGUYWPEvEjpWjcdVuG4FkBdKsQeytBQ6wJJGuj-jlweOQ5SA_kVyTE6896Da-LF7FNfEnOrlyTXUH0M766yKctLbro7ASgmXBy_pjgdcFdm5QE9Agk7Vy7hsjTMZYELXaxk"
                     frameBorder="0" scrolling="auto" allowFullScreen
-                    style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;">
+                    style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', maxWidth: '100%' }}>
             </iframe>
         </div>
     )
