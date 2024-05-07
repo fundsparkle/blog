eb7cdd8be10121bf205637eae9a5a19a7c84bae4
Przybysz <przybysz231@gmail.com>
2024-05-07 20:14:44 +0200

    fix newsletter page

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,18 +1,19 @@
+import React from "react";
+
 export default function NewsletterSignUp() {
     return (
-        <div className="container" style={{position: 'relative'}}>
-        <iframe width="100%"
-                src="https://6c25405f.sibforms.com/serve/MUIFAGh30jswQiwQXE3x0FqC2eIdS1d0Pfxu-7b0XcrMvJL9coqhxUkbyOD1w4uW-MrLMtvjGb7fpdGUYWPEvEjpWjcdVuG4FkBdKsQeytBQ6wJJGuj-jlweOQ5SA_kVyTE6896Da-LF7FNfEnOrlyTXUH0M766yKctLbro7ASgmXBy_pjgdcFdm5QE9Agk7Vy7hsjTMZYELXaxk"
+        <iframe id="myframe"
+                width="100%"
+                src="https://6c25405f.sibforms.com/serve/MUIFANbaQV41vEjf18IS4bqFBuFdH3Dud7yZPmkyfwM291qJS3gGS22f5NTL4f-twrXHm_DsUc-19jAQWWb40qxew3q00Yd_Sxlm6Btzy0nfo_3PEFb3I4551OZyYXlUNW4hko88Yn4gHfzsKNGO-FSptobRixJtWZyudnpJBRmcGkoM0Dg8TKO1tLdoSO4N2JwnIVZh0VfrKQWo"
                 frameBorder="0"
-                scrolling="auto"
+                allowTransparency={true}
                 style={{
                     display: 'block',
                     marginLeft: 'auto',
                     marginRight: 'auto',
-                    maxWidth: '80%',
-                    height: "100vh"
+                    maxWidth: '100%',
+                    height: "90vh",
                 }}>
         </iframe>
-        </div>
     )
 }
