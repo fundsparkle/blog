3d61539b08e14b90b887dca50994c036d4809df0
Przybysz <przybysz231@gmail.com>
2024-05-08 20:00:36 +0200

    update iframe src

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -7,7 +7,7 @@ export default function NewsletterSignUp() {
             <StyledWrapper>
                 <iframe id="myframe"
                         width="100%"
-                        src="https://6c25405f.sibforms.com/serve/MUIFAFxsDZkA8CONdjKE2kK0OHon-VRoreTtne0nb4jyaNW9-eBPJwBl1R0FUuYfmdcP9Ysl0yWdsCiqJ0wbBauo2V-_FDyw3fabjLsHoazDHxUOspykZeU6TlShuHOI2KQiOddxH-C9C4AKNcEMZHUx4tfb_c-v_QDI-qYSmScethjX8az8IEcjwQ5JzVX3UMd7qKKkacyBwkb_"
+                        src="https://6c25405f.sibforms.com/serve/MUIFACcvAwoDlFTYAcDD49deZAE3VePUj-RP36josJ8mTVRkpCpA4kzMmNUBsXUImF6plpQm9JzsC087SKYwgBZtIWGGdt_-IdHoyJ-MaD6r5tqy643LDWiw4QR9oy4vwUinoIqCL3Yy2K0KnWakZGbNM-kL9fgQauH8iV-R9STxzW694hnUI4151O0lIYx4zNNUEXuhxj31SGXS"
                         frameBorder={"0"}
                         style={{
                             display: 'block',
