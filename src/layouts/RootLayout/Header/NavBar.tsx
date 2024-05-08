6619dec5bd49fc9529f7636fd0d1868b5463f2a6
Przybysz <przybysz231@gmail.com>
2024-05-08 11:14:08 +0200

    change navbar name

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{id: 1, name: "Zapisz się do newslettera", to: "/newsletter"},
+  const links = [{id: 1, name: "Newsletter 📧", to: "/newsletter"},
       {id: 2, name: "O mnie", to: "/about"}]
   return (
     <StyledWrapper className="">
