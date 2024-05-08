78d50138961f10a44cb4f34c53b5d256c48873f6
Przybysz <przybysz231@gmail.com>
2024-05-08 11:02:00 +0200

    change newsletter iframe border | remove captcha

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -4,15 +4,16 @@ export default function NewsletterSignUp() {
     return (
         <iframe id="myframe"
                 width="100%"
-                src="https://6c25405f.sibforms.com/serve/MUIFAC6npXp-X1v11ErXQ5BBgXt2y9eqXsyGhzEqXnAbuSxOBGKL0rKt4B1_XN-8qnQGuoF_t1L9HaTMnrCDARvzALfuW_ch1oLkhtW1IY5fiG3QL-N9UwilgLClEjgbpnUVpCoYIbNBePCJ_wT5o7Yc0ZeojTJZtcg5iT_JC9Z2145FDoPqY8Txmi5WEpy6bQIQTkXMPw4i2NXt"
-                frameBorder="0"
+                src="https://6c25405f.sibforms.com/serve/MUIFAARNMHMZMG2wkbkFemz6skDxi_EVAbGqRxSpXiYgXh_7guMI0vNPomi0OT5_sefKFTmzqDrrXeYQrzkoYCoeW7yaszXjTWW6ApmH8lMXPVw51t6UM4Rwnmp815vWTpiE-AmcMxRR5wbxb69zVl4eMnX53rA3MObJ7DkNABg_2Gm8C8sqFNTwG5y02X3XJnMz-7MtlpEXNWMz"
                 allowTransparency={true}
+                frameBorder={"0"}
                 style={{
                     display: 'block',
                     marginLeft: 'auto',
                     marginRight: 'auto',
                     maxWidth: '100%',
                     height: "90vh",
+                    borderRadius: "2%"
                 }}>
         </iframe>
     )
