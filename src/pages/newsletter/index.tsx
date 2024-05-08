26d314d3ffe8d3a41e38158e2c843bbea98bfb1a
Przybysz <przybysz231@gmail.com>
2024-05-08 09:43:02 +0200

    add cookies bar from cookieyes

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 export default function NewsletterSignUp() {
     return (
         <iframe id="myframe"
                 width="100%"
-                src="https://6c25405f.sibforms.com/serve/MUIFANbaQV41vEjf18IS4bqFBuFdH3Dud7yZPmkyfwM291qJS3gGS22f5NTL4f-twrXHm_DsUc-19jAQWWb40qxew3q00Yd_Sxlm6Btzy0nfo_3PEFb3I4551OZyYXlUNW4hko88Yn4gHfzsKNGO-FSptobRixJtWZyudnpJBRmcGkoM0Dg8TKO1tLdoSO4N2JwnIVZh0VfrKQWo"
+                src="https://6c25405f.sibforms.com/serve/MUIFAOaqPQBXbc7Ujb7cYBOYXgE8hPNGuTzVztk_yFmTBdOj3Am5RUf66qBQmGDh-BBOv3C748D2jA8KMVqcUlVSyUHiFqAC2GebizTWNNVUCJYP_LmbIV1Sb_B-RgntAzYlZ4yu_Pq9zkSJInRG-dYwDHm6ZeRy6kA0es9d-ijgqo2_2MERgdV_BE7ezLHBPU_6J2WwYXlNaoOT"
                 frameBorder="0"
                 allowTransparency={true}
                 style={{
